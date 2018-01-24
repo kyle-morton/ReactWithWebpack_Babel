21364c123bee216587fedca1ffe64c434ef8193c
Kyle Morton <kylemortondevelopment@gmail.com>
2018-01-23 21:45:57 -0600

    checking in finished tutorial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# ReactWithWebpack_Babel" 
+# ReactWithWebpack_Babel 
+
+## Container/Presentational Principle
+
+carries all the logic -> functions for handling state changes, internal component state, etc.
+
+presentational components -> used just for displaying desired markup.
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: ["./src/js/app.js"], //takes app.js file with react components
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js/[name].js" //outputs compiled js file to dist/js folder
+  },
+  devServer: {
+    contentBase: './dist'
   },
   module: {
     rules: [
@@ -14,7 +18,21 @@ module.exports = {
         use: {
           loader: "babel-loader" //entry js file transformed using babel-loader (JSX into JS)
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
 };
